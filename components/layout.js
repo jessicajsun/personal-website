854c5dd0f506294271f07f3965390c70db6f1470
Jessica Sun <sunjessica17@gmail.com>
2020-06-14 21:50:39 -0500

    testing viewport

--- components/layout.js
+++ components/layout.js
@@ -6,6 +6,7 @@ export const siteTitle = 'Jessica Sun'
 
 export default function Layout() {
   return (
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <div className={styles.container}>
       <Head>
         <link rel="icon" href="/images/js.svg" />
