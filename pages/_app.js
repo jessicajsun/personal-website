01a08b482f1e7b0878f836fd68f880f5cc0fbff0
Jessica Sun <sunjessica17@gmail.com>
2020-06-24 15:34:27 -0500

    test analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import '../styles/global.css'
+import Router from "next/router";
+import withGA from "next-ga";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+export default withGA("UA-170720179-1", Router)(App);
